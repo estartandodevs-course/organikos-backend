3b4fb9cb005c140a0709f392ca06d52110662a40
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-10-12 01:56:38 -0300

    add router config

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
-const app = require('./config/app')
+const { app } = require('./config/app')
 require('dotenv').config()
 
 const port = process.env.PORT || 3001
+const { ProductRouter, UserRouter } = require('./api/routes/index')
+
+app.use('/api', UserRouter, ProductRouter)
+
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`)
 })
