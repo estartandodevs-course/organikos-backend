6cf7638b5636429d464709db00ac287041e6b9ad
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-10-13 20:01:08 -0300

    feat: add seller routers

---
+++ src/api/routes/seller/seller.router.js
@@ -0,0 +1,24 @@
+const { Router } = require('../../../config/app')
+
+const SellerRouter = Router()
+
+SellerRouter.get('/seller', async (req, res) => {
+    res.send('Listar todos os produtores')
+})
+SellerRouter.get('/seller/:id_tag', async (req, res) => {
+    res.send('Listar todos os produtores que vendem determinado produto')
+})
+SellerRouter.get('/seller/:id', (req, res) => {
+    res.send('Listar produtor pelo id')
+})
+SellerRouter.post('/seller', (req, res) => {
+    res.send('Cadastrar novo produtor')
+})
+SellerRouter.put('/seller/:id', (req, res) => {
+    res.send('Editar um produtor pelo id')
+})
+SellerRouter.delete('/seller/:id', (req, res) => {
+    res.send('Deleter um produtor pelo id')
+})
+
+module.exports = SellerRouter
--- src/server.js
+++ src/server.js
@@ -2,9 +2,13 @@ const { app } = require('./config/app')
 require('dotenv').config()
 
 const port = process.env.PORT || 3001
-const { ProductRouter, UserRouter } = require('./api/routes/index')
+const {
+    ProductRouter,
+    UserRouter,
+    SellerRouter,
+} = require('./api/routes/index')
 
-app.use('/api', UserRouter, ProductRouter)
+app.use('/api', UserRouter, ProductRouter, SellerRouter)
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`)
