6628669384c6c0847e3463d3f2d88075a3d09a41
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-10-18 11:01:05 -0300

    feat: add update controller and service classes to composer

--- src/api/composer/seller.compose.js
+++ src/api/composer/seller.compose.js
@@ -1,9 +1,20 @@
-const { PostNewSellerController } = require('../controller/seller/index');
-const { CreateSellerService } = require('../services/seller/index');
+const {
+    PostNewSellerController,
+    PutSellerByIdController,
+} = require('../controller/seller/index');
+const {
+    CreateSellerService,
+    UpdateSellerService,
+} = require('../services/seller/index');
 
 const createSellerService = new CreateSellerService();
 const postNewSellerController = new PostNewSellerController(
     createSellerService
+);
+
+const updateSellerService = new UpdateSellerService();
+const putSellerByIdController = new PutSellerByIdController(
+    updateSellerService
 );
 
 module.exports = { postNewSellerController };
