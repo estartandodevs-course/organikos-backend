fdde7d56dfbb099582972774942561344a058bd7
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-10-18 12:14:29 -0300

    feat: add get all seller classes instances

--- src/api/composer/seller.compose.js
+++ src/api/composer/seller.compose.js
@@ -1,20 +1,29 @@
 const {
     PostNewSellerController,
     PutSellerByIdController,
+    GetAllSellerController,
 } = require('../controller/seller/index');
+
 const {
     CreateSellerService,
     UpdateSellerService,
+    GetAllSellerService,
 } = require('../services/seller/index');
 
 const createSellerService = new CreateSellerService();
+const updateSellerService = new UpdateSellerService();
+const getAllSellerService = new GetAllSellerService();
+
 const postNewSellerController = new PostNewSellerController(
     createSellerService
 );
-
-const updateSellerService = new UpdateSellerService();
 const putSellerByIdController = new PutSellerByIdController(
     updateSellerService
 );
+const getAllSellerController = new GetAllSellerController(getAllSellerService);
 
-module.exports = { postNewSellerController, putSellerByIdController };
+module.exports = {
+    postNewSellerController,
+    putSellerByIdController,
+    getAllSellerController,
+};
