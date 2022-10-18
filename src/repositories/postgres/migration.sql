e338323ebd15d960e94c44ec3136739029222d24
ADRIANOSATO <94667621+ADRIANOSATO@users.noreply.github.com>
2022-10-18 02:53:52 -0300

    Delete migration.sql

--- src/repositories/postgres/migration.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-    id serial PRIMARY KEY,
-    name VARCHAR (255) NOT NULL,
-    email VARCHAR (50) NOT NULL,
-    phone INT (15) NOT NULL,
-    adress VARCHAR (255) NOT NULL
-);
