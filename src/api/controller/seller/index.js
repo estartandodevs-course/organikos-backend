6a964c574759fd6ca223b1b00176ff870cbd54cc
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-10-18 12:12:39 -0300

    feat: add get all controller to index

--- src/api/controller/seller/index.js
+++ src/api/controller/seller/index.js
@@ -1,7 +1,9 @@
 const PostNewSellerController = require('./postNewSeller.controller');
 const PutSellerByIdController = require('./putSellerById.controller');
+const GetAllSellerController = require('./getAll.controller');
 
 module.exports = {
     PostNewSellerController,
     PutSellerByIdController,
+    GetAllSellerController,
 };
