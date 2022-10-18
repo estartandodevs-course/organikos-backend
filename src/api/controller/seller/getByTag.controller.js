0109dec6f4032e0b216233dfcd8168befe54b426
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-10-18 18:33:21 -0300

    feat: add get seller by tag id controller logic

--- src/api/controller/seller/getByTag.controller.js
+++ src/api/controller/seller/getByTag.controller.js
@@ -1,11 +1,20 @@
 module.exports = class GetSellerByTagController {
+    constructor(service) {
+        this.seller = service;
+    }
     async execute(req, res) {
         try {
-            res.send(
-                'Listar todos os produtores que vendem determinado produto'
-            );
+            const seller = await this.seller.getByTag();
+            res.status(201).send(JSON.stringify(seller));
         } catch (error) {
-            res.status(500).send(error);
+            const errorObj = JSON.parse(error.message);
+            if (errorObj.statusCode) {
+                return res
+                    .status(errorObj.statusCode)
+                    .send(JSON.stringify(errorObj));
+            } else {
+                return res.status(500).send(error.message);
+            }
         }
     }
 };
