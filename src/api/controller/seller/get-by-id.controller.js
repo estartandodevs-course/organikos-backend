b59e9b0b6d4579be77a538ad0039e39708b7251c
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-10-18 18:38:43 -0300

    fix: new pattern to controller file names -- more human readble

---
+++ src/api/controller/seller/get-by-id.controller.js
@@ -0,0 +1,20 @@
+module.exports = class GetSellerByIdController {
+    constructor(service) {
+        this.seller = service;
+    }
+    async execute(req, res) {
+        try {
+            const seller = await this.seller.getById();
+            res.status(201).send(JSON.stringify(seller));
+        } catch (error) {
+            const errorObj = JSON.parse(error.message);
+            if (errorObj.statusCode) {
+                return res
+                    .status(errorObj.statusCode)
+                    .send(JSON.stringify(errorObj));
+            } else {
+                return res.status(500).send(error.message);
+            }
+        }
+    }
+};
