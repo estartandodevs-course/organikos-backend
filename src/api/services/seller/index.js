fa1c8c61523dbaef22b57be0c5c4be9ab5b7efc4
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-10-18 18:32:15 -0300

    feat: add get seller by tag id service

--- src/api/services/seller/index.js
+++ src/api/services/seller/index.js
@@ -1,9 +1,11 @@
 const CreateSellerService = require('./create-seller.service');
 const UpdateSellerService = require('./update-seller.service');
 const GetAllSellerService = require('./get-all-seller.service');
+const GetSellerByTagService = require('./get-by-tag.service');
 
 module.exports = {
     CreateSellerService,
     UpdateSellerService,
     GetAllSellerService,
+    GetSellerByTagService,
 };
