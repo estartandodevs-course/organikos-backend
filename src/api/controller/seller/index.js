1dd175654aea32f7a3dc01782dba176d35551299
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-10-18 10:59:54 -0300

    feat: add put controller to index

--- src/api/controller/seller/index.js
+++ src/api/controller/seller/index.js
@@ -1,5 +1,7 @@
 const PostNewSellerController = require('./postNewSeller.controller');
+const PutSellerByIdController = require('./putSellerById.controller');
 
 module.exports = {
     PostNewSellerController,
+    PutSellerByIdController,
 };
