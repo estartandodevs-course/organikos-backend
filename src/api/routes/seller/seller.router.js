6959c0c8f34cbdb414aaef20c73c1f516424e6a5
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-10-18 10:48:22 -0300

    fix: small changes to see new composer working

--- src/api/routes/seller/seller.router.js
+++ src/api/routes/seller/seller.router.js
@@ -1,21 +1,23 @@
 const { Router } = require('../../../config/app');
 const {
-    getAllController,
-    getByTagController,
-    getByIdController,
+    // getAllController,
+    // getByTagController,
+    // getByIdController,
     postNewSellerController,
-    putSellerByIdController,
-    deleteSellerByIdController,
-} = require('../../../api/controller/seller/index');
+    // putSellerByIdController,
+    // deleteSellerByIdController,
+} = require('../../../api/composer/seller.compose');
 
 const SellerRouter = Router();
 
-SellerRouter.get('/seller', getAllController.execute);
-SellerRouter.get('/seller/tag/:id_tag', getByTagController.execute);
-SellerRouter.get('/seller/:id', getByIdController.execute);
-SellerRouter.post('/seller', postNewSellerController.execute);
-SellerRouter.put('/seller/:id', putSellerByIdController.execute);
+// SellerRouter.get('/seller', getAllController.execute);
+// SellerRouter.get('/seller/tag/:id_tag', getByTagController.execute);
+// SellerRouter.get('/seller/:id', getByIdController.execute);
+SellerRouter.post('/seller', async (req, res) => {
+    await postNewSellerController.execute(req, res);
+});
+// SellerRouter.put('/seller/:id', putSellerByIdController.execute);
 
-SellerRouter.delete('/seller/:id', deleteSellerByIdController.execute);
+// SellerRouter.delete('/seller/:id', deleteSellerByIdController.execute);
 
 module.exports = SellerRouter;
