24505cbb98e1c843df831ef143a892c0ecf302d0
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-10-18 18:33:21 -0300

    feat: add get seller by tag id controller logic

--- src/api/controller/seller/index.js
+++ src/api/controller/seller/index.js
@@ -1,9 +1,11 @@
 const PostNewSellerController = require('./postNewSeller.controller');
 const PutSellerByIdController = require('./putSellerById.controller');
 const GetAllSellerController = require('./getAll.controller');
+const GetSellerByTagController = require('./getByTag.controller');
 
 module.exports = {
     PostNewSellerController,
     PutSellerByIdController,
     GetAllSellerController,
+    GetSellerByTagController,
 };
