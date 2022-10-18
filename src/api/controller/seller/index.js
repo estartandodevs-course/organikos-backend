5e2adb1fef92e1ee437d3636f0f490655cbed4c8
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-10-18 18:38:43 -0300

    fix: new pattern to controller file names -- more human readble

--- src/api/controller/seller/index.js
+++ src/api/controller/seller/index.js
@@ -1,7 +1,8 @@
-const PostNewSellerController = require('./postNewSeller.controller');
-const PutSellerByIdController = require('./putSellerById.controller');
-const GetAllSellerController = require('./getAll.controller');
-const GetSellerByTagController = require('./getByTag.controller');
+const PostNewSellerController = require('./post-new-seller.controller');
+const PutSellerByIdController = require('./put-seller-by-id.controller');
+const GetAllSellerController = require('./get-all.controller');
+const GetSellerByTagController = require('./get-by-tag-id.controller');
+const GetSellerByIdController = require('./get-by-id.controller');
 
 module.exports = {
     PostNewSellerController,
