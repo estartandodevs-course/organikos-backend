f72d1b2d09031bae488f7ce4fb37a9355c402af0
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-10-18 10:48:22 -0300

    fix: small changes to see new composer working

--- src/api/controller/seller/index.js
+++ src/api/controller/seller/index.js
@@ -1,22 +1,5 @@
-const GetAllSellerController = require('./getAll.controller');
-const GetSellerByTagController = require('./getByTag.controller');
-const GetSellerByIdController = require('./getById.controller');
 const PostNewSellerController = require('./postNewSeller.controller');
-const PutSellerByIdController = require('./putSellerById.controller');
-const DeleteSellerByIdController = require('./deleteSellerById.controller');
-
-const getAllController = new GetAllSellerController();
-const getByTagController = new GetSellerByTagController();
-const getByIdController = new GetSellerByIdController();
-const postNewSellerController = new PostNewSellerController();
-const putSellerByIdController = new PutSellerByIdController();
-const deleteSellerByIdController = new DeleteSellerByIdController();
 
 module.exports = {
-    getAllController,
-    getByTagController,
-    getByIdController,
-    postNewSellerController,
-    putSellerByIdController,
-    deleteSellerByIdController,
+    PostNewSellerController,
 };
