dcd08eb621852e24b14ee8bff7b20de1b09a1ee4
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-10-18 22:58:17 -0300

    feat: add delete seller service

--- src/api/services/seller/index.js
+++ src/api/services/seller/index.js
@@ -3,6 +3,7 @@ const UpdateSellerService = require('./update-seller.service');
 const GetAllSellerService = require('./get-all-seller.service');
 const GetSellerByTagService = require('./get-by-tag-id.service');
 const GetSellerByIdService = require('./get-by-id.service');
+const DeleteSellerService = require('./delete-seller.service');
 
 module.exports = {
     CreateSellerService,
@@ -10,4 +11,5 @@ module.exports = {
     GetAllSellerService,
     GetSellerByTagService,
     GetSellerByIdService,
+    DeleteSellerService,
 };
