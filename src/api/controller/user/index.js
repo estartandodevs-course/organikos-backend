519ef84111c392ebbf3d87deef137cc96c5eb995
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-10-23 13:51:10 -0300

    fix: import put controller file name!

--- src/api/controller/user/index.js
+++ src/api/controller/user/index.js
@@ -1,6 +1,6 @@
 const GetUserByIdController = require('./get-by-id.controller');
 const PostNewUserController = require('./post-new-user.controller');
-const PutUserByIdController = require('./put-by-id.controller');
+const PutUserByIdController = require('./put-user-by-id.controller');
 
 module.exports = {
     GetUserByIdController,
