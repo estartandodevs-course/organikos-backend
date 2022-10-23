5de3ff40e89944d65cc35eabbaf799bb78842a2a
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-10-23 13:48:39 -0300

    feat: add put/update user controllar

--- src/api/controller/user/index.js
+++ src/api/controller/user/index.js
@@ -1,7 +1,9 @@
 const GetUserByIdController = require('./get-by-id.controller');
 const PostNewUserController = require('./post-new-user.controller');
+const PutUserByIdController = require('./put-by-id.controller');
 
 module.exports = {
     GetUserByIdController,
     PostNewUserController,
+    PutUserByIdController,
 };
