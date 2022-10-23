d21467ec03790f2203d3b9da7530a8ce7b8bba5e
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-10-23 13:47:21 -0300

    feat: update user service

--- src/api/services/user/index.js
+++ src/api/services/user/index.js
@@ -1,6 +1,8 @@
 const CreateUserService = require('./create-user.service');
 const GetUserByIdService = require('./get-by-id.service');
+const UpdateUserService = require('./update-user.service');
 module.exports = {
     CreateUserService,
     GetUserByIdService,
+    UpdateUserService,
 };
