1d561c919c0ed75e41e3d48b3182443a20388e91
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-10-23 13:41:53 -0300

    feat: add post new user controller

--- src/api/controller/user/index.js
+++ src/api/controller/user/index.js
@@ -1,5 +1,7 @@
 const GetUserByIdController = require('./get-by-id.controller');
+const PostNewUserController = require('./post-new-user.controller');
 
 module.exports = {
     GetUserByIdController,
+    PostNewUserController,
 };
