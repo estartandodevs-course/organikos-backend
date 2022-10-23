3caf42d02c76ae7c573e1fc9e36e6fc5ccb5acae
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-10-23 13:40:20 -0300

    feat: add create user service

---
+++ src/api/services/user/create-user.service.js
@@ -0,0 +1,27 @@
+const generateId = require('../../helpers/id-generator.helper');
+
+module.exports = class CreateUserService {
+    constructor(repository) {
+        this.repository = repository;
+    }
+    async create(params) {
+        try {
+            const { name, phone, email, password, address } = params;
+
+            if (!name) throw new Error('Missing name');
+            if (!phone) throw new Error('Missing phone');
+            if (!email) throw new Error('Missing email');
+            if (!password) throw new Error('Missing password');
+            if (!address) throw new Error('Missing address');
+
+            return {
+                id: generateId(),
+                ...params,
+            };
+        } catch (error) {
+            throw new Error(
+                JSON.stringify({ error: error.message, statusCode: 400 })
+            );
+        }
+    }
+};
