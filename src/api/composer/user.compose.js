46c3aabf32412ece621330444e5ced8087f86a68
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-10-23 13:42:51 -0300

    feat: add create user composer

--- src/api/composer/user.compose.js
+++ src/api/composer/user.compose.js
@@ -1,13 +1,23 @@
-const { GetUserByIdController } = require('../controller/user/index');
+const {
+    GetUserByIdController,
+    PostNewUserController,
+} = require('../controller/user/index');
 
-const { GetUserByIdService } = require('../services/user/index');
+const {
+    GetUserByIdService,
+    CreateUserService,
+} = require('../services/user/index');
 
 const UserRepository = require('../repository/user.repository');
-
 const userRepository = new UserRepository();
+
 const getUserByIdService = new GetUserByIdService(userRepository);
 const getUserByIdController = new GetUserByIdController(getUserByIdService);
 
+const createUserService = new CreateUserService(userRepository);
+const postNewUserController = new PostNewUserController(createUserService);
+
 module.exports = {
     getUserByIdController,
+    postNewUserController,
 };
