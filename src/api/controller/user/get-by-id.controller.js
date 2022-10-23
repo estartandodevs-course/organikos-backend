9d8507b62d94622c2e07e75854be12e9ba39b4ee
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-10-23 13:26:04 -0300

    feat: add get user by id controller

---
+++ src/api/controller/user/get-by-id.controller.js
@@ -0,0 +1,20 @@
+module.exports = class GetUserByIdController {
+    constructor(service) {
+        this.user = service;
+    }
+    async execute(req, res) {
+        try {
+            const user = await this.user.getById();
+            res.status(200).send(JSON.stringify(user));
+        } catch (error) {
+            const errorObj = JSON.parse(error.message);
+            if (errorObj.statusCode) {
+                return res
+                    .status(errorObj.statusCode)
+                    .send(JSON.stringify(errorObj));
+            } else {
+                return res.status(500).send(error.message);
+            }
+        }
+    }
+};
