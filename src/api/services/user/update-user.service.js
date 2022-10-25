50d22948399bfc46072912a1d393437f5b7e68dd
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-10-24 22:54:11 -0300

    fix: update essential parameters

--- src/api/services/user/update-user.service.js
+++ src/api/services/user/update-user.service.js
@@ -1,21 +1,30 @@
+const isValidUUID = require('../../helpers/uuid-validator.helper');
 module.exports = class UpdateUserService {
     constructor(repository) {
         this.repository = repository;
     }
-    async update(params) {
+    async update(params, id) {
         try {
             const { name, phone, email, password, address } = params;
 
-            // const { city, state, country, street, number, zipCode } =
-            //     params.address;
-
+            if (!isValidUUID(id)) throw new Error('Id is not a valid uuid id');
             if (!name) throw new Error('Missing name');
             if (!phone) throw new Error('Missing phone');
             if (!email) throw new Error('Missing email');
             if (!password) throw new Error('Missing password');
             if (!address) throw new Error('Missing address');
 
-            return 'user';
+            const user = await this.repository.update({
+                userId: id,
+                contact: {
+                    name,
+                    phone,
+                    email,
+                    password,
+                    address,
+                },
+            });
+            return user;
         } catch (error) {
             throw new Error(
                 JSON.stringify({ error: error.message, statusCode: 400 })
