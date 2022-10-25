193fd71a3f2065626bb55b73cf17332dea13d725
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-10-24 23:04:13 -0300

    fix: throwing error if id is not a valid uuid

--- src/api/services/user/delete-user.service.js
+++ src/api/services/user/delete-user.service.js
@@ -1,9 +1,11 @@
+const isValidUUID = require('../../helpers/uuid-validator.helper');
 module.exports = class DeleteUserService {
     constructor(repository) {
         this.repository = repository;
     }
     async delete(id) {
         try {
+            if (!isValidUUID(id)) throw new Error('Id is not a valid uuid id');
             await this.repository.delete(id);
             return { id: id };
         } catch (error) {
--- src/api/services/user/get-by-id.service.js
+++ src/api/services/user/get-by-id.service.js
@@ -1,9 +1,11 @@
+const isValidUUID = require('../../helpers/uuid-validator.helper');
 module.exports = class GetUserByIdService {
     constructor(repository) {
         this.repository = repository;
     }
     async getById(id) {
         try {
+            if (!isValidUUID(id)) throw new Error('Id is not a valid uuid id');
             return [
                 {
                     userId: id,
