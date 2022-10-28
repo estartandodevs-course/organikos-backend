63cc1eb07a58b3bd9cb038650af8e31fd67cb319
ADRIANOSATO <adrianosato_84@yahoo.com.br>
2022-10-28 16:55:38 -0300

    add models seller

--- src/api/repository/index.js
+++ src/api/repository/index.js
@@ -1,6 +1,10 @@
 const Sequelize = require('sequelize');
 const configDB = require('./database');
 
+const Seller = require('./models/Seller');
+
 const connection = new Sequelize(configDB);
+
+Seller.init(connection);
 
 module.exports = connection;
