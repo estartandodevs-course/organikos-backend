d0a115a277aa0f116c0a3eb465c44c2ed93f3d41
Ramon <ramon1234rxm@gmail.com>
2022-11-01 16:18:11 -0300

    chore: adding test connection sequelize

--- src/api/repository/index.js
+++ src/api/repository/index.js
@@ -5,6 +5,17 @@ const Seller = require('./models/Seller');
 
 const connection = new Sequelize(configDB);
 
+async function connectionTest() {
+    try {
+        await Sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+connectionTest();
+
 Seller.init(connection);
 
 module.exports = connection;
