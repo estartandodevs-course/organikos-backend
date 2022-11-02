e11159d9a0cf430db01100cce4f12e63ac0d03aa
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-11-02 01:55:34 -0300

    feat: add seller models

---
+++ src/api/database/models/seller.model.js
@@ -0,0 +1,44 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../../../config/database');
+
+const Seller = sequelize.define('seller', {
+    id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    desc: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    payment: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    delivery: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    tag: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    wpp: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+});
+// Seller.sync();
+module.exports = Seller;
