2b126dc7ca4c6f86ba997755c8766f6d903c996c
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-11-02 18:18:11 -0300

    feat: add get product by name controller

--- src/api/controllers/product/index.js
+++ src/api/controllers/product/index.js
@@ -1,7 +1,9 @@
 const GetProductBySellerIdController = require('./get-by-seller-id.controller');
 const PostNewProductController = require('./post-new-product.controller');
+const GetProductByNameController = require('./get-by-name.controller');
 
 module.exports = {
     GetProductBySellerIdController,
     PostNewProductController,
+    GetProductByNameController,
 };
