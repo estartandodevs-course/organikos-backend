160b94063fc8fa0e0d8c192757a0251afa5d3b19
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-11-01 21:13:04 -0300

    feat: postgres orm strucutre

--- src/api/database/index.js
+++ src/api/database/index.js
@@ -1,13 +1,23 @@
 const Sequelize = require('sequelize');
-const configDB = require('./database');
+// const configDB = require('./database');
 
 const Seller = require('./models/Seller');
 
-const connection = new Sequelize(configDB);
+const connection = new Sequelize(
+    'postgres://estartandodev:Lqned1clI6YBTsnNe1X1frh0cHOzZL6g@dpg-cdgoodkgqg47bmbi5fhg-a.oregon-postgres.render.com/seller',
+    {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        dialectOptions: {
+            ssl: true,
+            native: true,
+        },
+    }
+);
 
 async function connectionTest() {
     try {
-        await Sequelize.authenticate();
+        await connection.authenticate();
         console.log('Connection has been established successfully.');
     } catch (error) {
         console.error('Unable to connect to the database:', error);
