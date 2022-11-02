130a6202e1aa8c91a2c92898b179338c7e4694d9
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-11-02 18:10:50 -0300

    feat: add get product by name

--- src/api/services/product/index.js
+++ src/api/services/product/index.js
@@ -1,7 +1,8 @@
 const GetProductBySellerIdService = require('./get-by-seller-id.service');
 const CreateProductService = require('./create-product.service');
-
+const GetProductByNameService = require('./get-by-name.service');
 module.exports = {
     GetProductBySellerIdService,
     CreateProductService,
+    GetProductByNameService,
 };
