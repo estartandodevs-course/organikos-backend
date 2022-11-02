f7399b68daca5e46317feacca5a512815917357d
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-11-02 01:57:06 -0300

    fix: update logic

--- src/api/services/seller/create-seller.service.js
+++ src/api/services/seller/create-seller.service.js
@@ -12,7 +12,7 @@ module.exports = class CreateSellerService {
                 phone,
                 email,
                 password,
-                address,
+                // address,
                 distribution,
                 payment,
                 category,
@@ -23,26 +23,42 @@ module.exports = class CreateSellerService {
             if (!phone) throw new Error('Missing phone');
             if (!email) throw new Error('Missing email');
             if (!password) throw new Error('Missing password');
-            if (!address) throw new Error('Missing address');
+            // if (!address) throw new Error('Missing address');
             if (!distribution) throw new Error('Missing distribution');
             if (!payment) throw new Error('Missing payment');
             if (!category) throw new Error('Missing category');
+            const { number, isWpp } = phone;
+            const id = generateId();
+            const wwp = isWpp ? 1 : 0;
 
-            const user = await this.repository.create({
-                sellerId: generateId(),
-                contact: {
-                    name,
-                    desc,
-                    phone,
-                    email,
-                    password,
-                },
-                address,
-                distribution,
-                payment,
-                category,
+            const paymentToString = payment.join();
+            const distributionToString = distribution.join();
+            const categoryToString = category.join();
+
+            const seller = await this.repository.create({
+                id: id,
+                name: name,
+                email: email,
+                password: password,
+                phone: number,
+                desc: desc,
+                payment: paymentToString,
+                delivery: distributionToString,
+                tag: categoryToString,
+                wpp: wwp,
             });
-            return user;
+
+            return {
+                id: seller.id,
+                name: seller.name,
+                email: seller.email,
+                phone: seller.phone,
+                desc: seller.desc,
+                payment: seller.payment.split(','),
+                delivery: seller.delivery.split(','),
+                tag: seller.tag.split(','),
+                wpp: seller.wpp == 1 ? true : false,
+            };
         } catch (error) {
             throw new Error(
                 JSON.stringify({ error: error.message, statusCode: 400 })
