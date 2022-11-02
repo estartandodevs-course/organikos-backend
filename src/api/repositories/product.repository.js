444a7ecb7d612a721cf515187879a2bc166020fa
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-11-02 17:06:58 -0300

    feat: add product repository

---
+++ src/api/repositories/product.repository.js
@@ -0,0 +1,37 @@
+const Product = require('../database/models/seller.model');
+
+module.exports = class ProductRepository {
+    constructor() {}
+    async create(data) {
+        try {
+            const { id, name, price, measure, status, category } = data;
+            const product = {
+                id_seller: id,
+                name: name,
+                price,
+                measure,
+                status,
+                category,
+            };
+            return await Product.create(product);
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+
+    async getById(id) {
+        try {
+            return await Product.findAll({ where: { id_seller: id } });
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+
+    async getByCategory(cat) {
+        try {
+            return await Product.findOne({ where: { category: cat } });
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+};
