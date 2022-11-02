60894802ba48890ea7f93be957b568f85c469e2c
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-11-02 18:06:08 -0300

    feat: add create product service

--- src/api/services/product/index.js
+++ src/api/services/product/index.js
@@ -1,5 +1,7 @@
 const GetProductBySellerIdService = require('./get-by-seller-id.service');
+const CreateProductService = require('./create-product.service');
 
 module.exports = {
     GetProductBySellerIdService,
+    CreateProductService,
 };
