a476da720c417dfa3096f6862ebe362500d96fe5
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-11-03 23:31:24 -0300

    fix: fix returns

--- src/api/controllers/product/index.js
+++ src/api/controllers/product/index.js
@@ -1,9 +1,11 @@
 const GetProductBySellerIdController = require('./get-by-seller-id.controller');
 const PostNewProductController = require('./post-new-product.controller');
 const GetProductByNameController = require('./get-by-name.controller');
+const GetProductByTagController = require('./get-by-tag.controller');
 
 module.exports = {
     GetProductBySellerIdController,
     PostNewProductController,
     GetProductByNameController,
+    GetProductByTagController,
 };
--- src/api/services/seller/get-all-seller.service.js
+++ src/api/services/seller/get-all-seller.service.js
@@ -4,8 +4,33 @@ module.exports = class GetAllSellerService {
     }
     async getAll() {
         try {
-            console.log('oi');
-            return await this.repository.getAll();
+            const users = await this.repository.getAll();
+            return users.map((user) => {
+                const payment = user.payment.split(',');
+                const delivery = user.delivery.split(',');
+                const tag = user.tag.split(',');
+
+                const userObj = {
+                    sellerId: user.id,
+                    contact: {
+                        name: user.name,
+                        email: user.email,
+                        desc: user.desc,
+                        phone: {
+                            number: user.phone,
+                            isWpp: user.wpp == 1 ? true : false,
+                        },
+                    },
+                    adress: {},
+                    distribution: delivery,
+                    payment: payment,
+                    category: tag,
+                    history: [],
+                    rating: '',
+                    certificate: user.certificate == 1 ? true : false,
+                };
+                return userObj;
+            });
         } catch (error) {
             throw new Error(
                 JSON.stringify({ error: error.message, statusCode: 500 })
