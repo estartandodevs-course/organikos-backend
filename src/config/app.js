75cba887f9e3bcd703301ca787136479752f0eac
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-11-06 13:05:54 -0300

    feat: add cors

--- src/config/app.js
+++ src/config/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const Router = express.Router;
@@ -7,5 +8,13 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Organikos API!');
 });
+
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 module.exports = { app, Router };
