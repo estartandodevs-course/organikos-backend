1a01069723c787097efd24af2a332d422ff2668a
Pedro Bueno <97570298+pedrobuenoxs@users.noreply.github.com>
2022-11-08 19:54:04 -0300

    Delete img-database.js

--- src/config/img-database.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require('cloudinary').v2;
-
-cloudinary.config({
-    cloud_name: 'dwd73t7dm',
-    api_key: '895988937998262',
-    api_secret: '_HX76mfpfPmCjKb7ZzTqk238ZgA',
-});
-
-module.exports = cloudinary;
