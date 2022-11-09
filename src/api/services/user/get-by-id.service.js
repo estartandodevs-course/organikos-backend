1b6de21d720d1849b83b418349b7ce2bc5c88eaf
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-11-09 20:06:49 -0300

    feat: create get user by id service

--- src/api/services/user/get-by-id.service.js
+++ src/api/services/user/get-by-id.service.js
@@ -6,7 +6,22 @@ module.exports = class GetUserByIdService {
     async getById(id) {
         try {
             if (!isValidUUID(id)) throw new Error('Id is not a valid uuid id');
+            const { user, address } = await this.repository.getById(id);
+            return {
+                name: user.name,
+                phone: user.phone,
+                email: user.email,
+                address: {
+                    street: address.street,
+                    number: address.number,
+                    complement: address.complement,
+                    city: address.city,
+                    state: address.state,
+                    zipCode: address.zipCode,
+                },
+            };
         } catch (error) {
+            console.log(error);
             throw new Error(
                 JSON.stringify({ error: error.message, statusCode: 400 })
             );
